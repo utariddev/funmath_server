a07e4b04e2a611a355af87e955fde19d722fec0c
utarid <utarid@protonmail.com>
2023-06-15 14:35:42 +0200

    initial commit

---
+++ src/main/java/org/utarid/funmath/FunMathApplication.java
@@ -0,0 +1,13 @@
+package org.utarid.funmath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunMathApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunMathApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/utarid/funmath/FunMathApplicationTests.java
@@ -0,0 +1,13 @@
+package org.utarid.funmath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunMathApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
